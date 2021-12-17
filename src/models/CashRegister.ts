17e58c6dad2e05aebd8acd1eeb5ecce7c174a616
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2021-12-17 13:01:50 +0100

    feat: add Cashier model, rename old one to CashRegister

---
+++ src/models/CashRegister.ts
@@ -0,0 +1,33 @@
+import { Order } from "./Order";
+import { Payment } from "./Payment";
+
+export class CashRegister {
+  private _orders: Order[] = new Array<Order>(2);
+  private _currentOrderId: number = 0;
+
+  get currentOrder() {
+    return this._orders[this._currentOrderId];
+  }
+
+  pauseOrder() {
+    this._currentOrderId = 1;
+  }
+
+  resumeOrder() {
+    this._currentOrderId = 0;
+  }
+
+  pay(payment: Payment): boolean {
+    this.currentOrder.addPayment(payment);
+
+    if (
+      this.currentOrder.getTotalPaid() >= this.currentOrder.card.getTotalPrice()
+    ) {
+      this.currentOrder.state = Order.State.PAID;
+
+      return true;
+    }
+
+    return false;
+  }
+}
