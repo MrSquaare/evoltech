f55edad63eb84b3bc9e9392cbd7ef35b740147b0
Abel Pouillet <abelix.pouillet@gmail.com>
2021-12-17 14:21:49 +0100

    contenu boutton PaymentModal

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "./styles/app.scss";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import PaymentModal from "./components/cashier/PaymentModal";
 import Cashier from "./pages/Cashier";
 import Login from "./pages/Login";
-import PaymentModal from "./components/cashier/PaymentModal";
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Cashier />} />
-        <Route path="/devpm" element={<PaymentModal />} />
+        <Route path="/devpm" element={<PaymentModal price="40" />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App ;
+export default App;
--- src/components/cashier/PaymentModal.tsx
+++ src/components/cashier/PaymentModal.tsx
@@ -1,11 +1,14 @@
 import { Box, Button, Typography } from "@mui/material";
 import React, { FunctionComponent } from "react";
 
-interface OwnProps {}
+interface OwnProps {
+  price: string;
+}
 
 type Props = OwnProps;
 
 const PaymentModal: FunctionComponent<Props> = (props) => {
+  const { price } = props;
   return (
     <Box>
       <Typography>Sélectionnez un ou des moyens de paiement</Typography>
@@ -18,9 +21,10 @@ const PaymentModal: FunctionComponent<Props> = (props) => {
         <Button>Payer une partie</Button>
       </Box>
       <Box>
-        <Button></Button>
-        <Button></Button>
-        <Button></Button>
+        <Button> Retour </Button>
+        <Button> à payer : {price} </Button>
+        <Button> Payer une partie</Button>
+        <Button> Payer</Button>
       </Box>
     </Box>
   );
