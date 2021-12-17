6013859bedfc0812376a1c13ea5fa30bc4015d28
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2021-12-17 13:38:48 +0100

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import "./styles/app.scss";
 
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Cashier from "./pages/Cashier";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
-      <Cashier />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Cashier />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
