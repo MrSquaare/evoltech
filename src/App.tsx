51d9887f52f5930c7b80f4129894007a8b571124
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2021-12-17 13:46:56 +0100

    feat: update page component names

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import "./styles/app.scss";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Cashier from "./pages/Cashier";
-import Login from "./pages/Login";
+import CashierPage from "./pages/Cashier";
+import LoginPage from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Cashier />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<CashierPage />} />
       </Routes>
     </BrowserRouter>
   );
