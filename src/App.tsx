ace5cebb0d0a386b85ea1688bd82cc8495944902
RandyLarzabal <71065214+RandyLarzabal@users.noreply.github.com>
2022-01-06 11:02:50 +0100

    feat:pdm
    Ajout de la modal avec les props

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,18 @@ import "./styles/app.scss";
 
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import ProductDeleteModal from "./components/cashier/ProductDeleteModal";
 import CashierPage from "./pages/Cashier";
 import { FSMPage } from "./pages/FSM";
 import LoginPage from "./pages/Login";
 
 function App() {
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/fsm" element={<FSMPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<CashierPage />} />
-
-        {"dev"}
-        <Route path={"/dev/pdm"} element={<ProductDeleteModal />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/cashier/ProductDeleteModal.tsx
+++ src/components/cashier/ProductDeleteModal.tsx
@@ -1,69 +1,83 @@
-import { Box, Button, Paper, Typography } from "@mui/material";
+import { Box, Button, Modal, Paper, Typography } from "@mui/material";
 import React, { FunctionComponent } from "react";
 
-interface OwnProps {}
+interface OwnProps {
+  open: boolean;
+  handleClose: () => void;
+}
 
 type Props = OwnProps;
 
 const ProductDeleteModal: FunctionComponent<Props> = (props) => {
+  const { open, handleClose } = props;
   return (
-    <Paper
-      elevation={3}
-      sx={{
-        width: "fit-content",
-        padding: "32px",
-        maxWidth: "500px",
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%,-50%)",
-      }}
-    >
-      <Typography
-        color={"#1B2F46"}
-        fontFamily={"Poppins"}
-        fontSize={24}
-        textAlign={"center"}
-        fontWeight={500}
+    <>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
-        Êtes-vous sûr de vouloir supprimer ce produit?
-      </Typography>
-      <Box
-        className={"button-container"}
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginTop: "24px",
-        }}
-      >
-        <Button
+        <Paper
+          elevation={3}
           sx={{
-            backgroundColor: "#607790",
-            ":hover": {
-              backgroundColor: "#607790",
-            },
-            color: "white",
-            fontFamily: "poppins",
-            fontSize: "18px",
+            width: "fit-content",
+            padding: "32px",
+            maxWidth: "500px",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%,-50%)",
           }}
         >
-          {"<"} Retour
-        </Button>
-        <Button
-          sx={{
-            backgroundColor: "#FC997C",
-            ":hover": {
-              backgroundColor: "#FC997C",
-            },
-            color: "white",
-            fontFamily: "poppins",
-            fontSize: "18px",
-          }}
-        >
-          Confirmer
-        </Button>
-      </Box>
-    </Paper>
+          <Typography
+            color={"#1B2F46"}
+            fontFamily={"Poppins"}
+            fontSize={24}
+            textAlign={"center"}
+            fontWeight={500}
+          >
+            Êtes-vous sûr de vouloir supprimer ce produit?
+          </Typography>
+          <Box
+            className={"button-container"}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: "24px",
+            }}
+          >
+            <Button
+              sx={{
+                backgroundColor: "#607790",
+                ":hover": {
+                  backgroundColor: "#607790",
+                },
+                color: "white",
+                fontFamily: "poppins",
+                fontSize: "18px",
+              }}
+              onClick={handleClose}
+            >
+              {"<"} Retour
+            </Button>
+            <Button
+              sx={{
+                backgroundColor: "#FC997C",
+                ":hover": {
+                  backgroundColor: "#FC997C",
+                },
+                color: "white",
+                fontFamily: "poppins",
+                fontSize: "18px",
+              }}
+            >
+              Confirmer
+            </Button>
+          </Box>
+        </Paper>
+      </Modal>
+    </>
   );
 };
 
