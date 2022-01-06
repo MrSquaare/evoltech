8cc2a8ed1cbb9c6c98404e3d73b70ccf2da31568
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2022-01-06 11:45:04 +0100

    feat: add logic to simulate open cashier, wrap cashier open component into modal

--- src/components/cashier/CashierOpenModal.tsx
+++ src/components/cashier/CashierOpenModal.tsx
@@ -1,52 +1,58 @@
 import CancelIcon from "@mui/icons-material/Cancel";
-import { Paper, Typography } from "@mui/material";
+import { Modal, Paper, Typography } from "@mui/material";
 import React, { FunctionComponent } from "react";
 
-interface OwnProps {}
+interface OwnProps {
+  open: boolean;
+  handleClose: () => void;
+}
 
 type Props = OwnProps;
 
 const CashierOpenModal: FunctionComponent<Props> = (props) => {
+  const { open, handleClose } = props;
   return (
-    <Paper
-      elevation={3}
-      sx={{
-        width: "fit-content",
-        padding: "32px",
-        maxWidth: "500px",
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%,-50%)",
-        textAlign: "center",
-      }}
-    >
-      <CancelIcon
+    <Modal open={open} onClose={handleClose}>
+      <Paper
+        elevation={3}
         sx={{
-          width: "237px",
-          height: "237px",
-          color: "#f44337",
+          width: "fit-content",
+          padding: "32px",
+          maxWidth: "500px",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%,-50%)",
+          textAlign: "center",
         }}
-      />
-      <Typography
-        color={"#1B2F46"}
-        fontFamily={"Poppins"}
-        fontSize={48}
-        textAlign={"center"}
-        fontWeight={700}
       >
-        Caisse ouverte
-      </Typography>
-      <Typography
-        color={"#1B2F46"}
-        fontFamily={"Poppins"}
-        fontSize={24}
-        textAlign={"center"}
-        fontWeight={500}
-      >
-        Veuillez fermer le tiroir pour continuer à utiliser la caisse
-      </Typography>
-    </Paper>
+        <CancelIcon
+          sx={{
+            width: "237px",
+            height: "237px",
+            color: "#f44337",
+          }}
+        />
+        <Typography
+          color={"#1B2F46"}
+          fontFamily={"Poppins"}
+          fontSize={48}
+          textAlign={"center"}
+          fontWeight={700}
+        >
+          Caisse ouverte
+        </Typography>
+        <Typography
+          color={"#1B2F46"}
+          fontFamily={"Poppins"}
+          fontSize={24}
+          textAlign={"center"}
+          fontWeight={500}
+        >
+          Veuillez fermer le tiroir pour continuer à utiliser la caisse
+        </Typography>
+      </Paper>
+    </Modal>
   );
 };
 
