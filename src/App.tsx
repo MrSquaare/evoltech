317dfad0e4ac455f1aa5b0b56287ddb81e0afc7e
RandyLarzabal <71065214+RandyLarzabal@users.noreply.github.com>
2022-01-06 10:20:14 +0100

    feat:pdm
    Ajout du product modal delete

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./styles/app.scss";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import ProductDeleteModal from "./components/cashier/ProductDeleteModal";
 import CashierPage from "./pages/Cashier";
 import { FSMPage } from "./pages/FSM";
 import LoginPage from "./pages/Login";
@@ -14,6 +15,9 @@ function App() {
         <Route path="/fsm" element={<FSMPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<CashierPage />} />
+
+        {"dev"}
+        <Route path={"/dev/pdm"} element={<ProductDeleteModal />} />
       </Routes>
     </BrowserRouter>
   );
